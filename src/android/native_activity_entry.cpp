4db4a23a8c507c9a45ac850c4618558e53d821b8
Breezyslasher <167659924+Breezyslasher@users.noreply.github.com>
2026-03-26 16:25:18 -0400

    android: initialize SDL bootstrap in NativeActivity entry

--- src/android/native_activity_entry.cpp
+++ src/android/native_activity_entry.cpp
@@ -1,6 +1,8 @@
 #ifdef __ANDROID__
 
 #include <android/native_activity.h>
+#include <dlfcn.h>
+#include <jni.h>
 #include <cstddef>
 
 extern int main(int argc, char* argv[]);
@@ -10,6 +12,26 @@ void ANativeActivity_onCreate(ANativeActivity* activity, void* savedState, size_
     (void)activity;
     (void)savedState;
     (void)savedStateSize;
+
+    using SDLAndroidInitFn = void (*)(JNIEnv*, jclass);
+    using SDLSetMainReadyFn = void (*)();
+
+    void* sdlHandle = dlopen("libSDL2.so", RTLD_NOW | RTLD_NOLOAD);
+    if (!sdlHandle) {
+        sdlHandle = dlopen("libSDL2.so", RTLD_NOW);
+    }
+
+    if (sdlHandle && activity && activity->vm && activity->clazz) {
+        auto sdlAndroidInit = reinterpret_cast<SDLAndroidInitFn>(dlsym(sdlHandle, "SDL_Android_Init"));
+        auto sdlSetMainReady = reinterpret_cast<SDLSetMainReadyFn>(dlsym(sdlHandle, "SDL_SetMainReady"));
+        if (sdlAndroidInit && sdlSetMainReady) {
+            JNIEnv* env = nullptr;
+            if (activity->vm->AttachCurrentThread(&env, nullptr) == JNI_OK && env) {
+                sdlAndroidInit(env, reinterpret_cast<jclass>(activity->clazz));
+                sdlSetMainReady();
+            }
+        }
+    }
 
     char* argv[] = { (char*)"VitaSuwayomi", nullptr };
     main(1, argv);
